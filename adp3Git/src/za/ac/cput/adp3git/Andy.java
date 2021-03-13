3368e08e2357e74bc3345062d8e1b5a720f364ad
Andy Hine <62076750+AndyHine@users.noreply.github.com>
2021-03-13 10:50:55 +0200

    Add files via upload

---
+++ adp3Git/src/za/ac/cput/adp3git/Andy.java
@@ -0,0 +1,25 @@
+package za.ac.cput.adp3git;
+
+public class Andy
+{
+    /**
+     * this returns the lenght of a string
+     *
+     * @param str
+     * @return
+     */
+    public int lengthOfString(String str)
+    {
+        if (str == null|| str.isEmpty())
+            return 0;
+        return str.length();
+    }
+
+    /**
+     * Main method
+     * @param args
+     */
+    public static void  main (String[]args){
+        System.out.println("git class");
+    }
+}
